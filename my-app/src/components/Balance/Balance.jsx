083ac0c23fb344ccc69cd7c46e27997d0efbe1d9
GrietM <marcelagriet@gmail.com>
2021-06-23 16:36:18 -0300

    Conexiones con el Back para construir el balance en pag 'Home'

--- my-app/src/components/Balance/Balance.jsx
+++ my-app/src/components/Balance/Balance.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Descriptions } from 'antd';
 import { getTimeProps } from 'antd/lib/date-picker/generatePicker';
+import axios from 'axios'
 
 const Balance = () => {
-  /* let totalIncomes= axios.get(ruta del incomes)
-  let totalExpenses= axios.get(ruta del expenses)
-  let balance = totalIncomes-totalExpenses */
-  
-  let totalExpenses = 200000
-  let totalIncomes = 320000
-  let balance = totalIncomes-totalExpenses 
-  //let date= new Date()
-  
+
+  const [balance, setBalance] = useState([])
+  const [totalIncomes, setTotalIncomes]= useState([])
+  const [totalExpenses, setTotalExpenses]= useState([])
+
+  const token = localStorage.getItem('Token') 
+  const getOperationsBalance = async () => {
+    try{
+      const resp = await axios.get('http://localhost:8080/api/admin/balance',{headers: {Authorization: 'Bearer ' + token}});
+      console.log("resp.data",resp.data)
+      console.log(",resp.data.totalIncomes",resp.data.totalIncomes)
+      console.log(",resp.data.totalExpenses",resp.data.totalExpenses)
+      console.log("resp.data.balance",resp.data.balance)
+      let totalExpenses= resp.data.totalExpenses
+      let totalIncomes= resp.data.totalIncomes
+      let balance = resp.data.balance
+      setBalance(balance) 
+      setTotalIncomes(totalIncomes)
+      setTotalExpenses(totalExpenses)
+    }
+    catch(error){
+        //localStorage.removeItem('Token')
+        //GoToMain()   
+        //message.error("Sesión expirada. Inicie sesión nuevamente", 4)
+        throw error        
+    }}
+ 
+    //let date= new Date()
+     
+  useEffect(() =>{
+    getOperationsBalance()
+},[]
+)
     return (
   <Descriptions title="Current Balance" style= {{fontWeight:'bolder'}}>
-    <Descriptions.Item label="UserName" style= {{fontWeight:'bolder'}}> Marce Griet</Descriptions.Item>
-    <Descriptions.Item label="Total Incomes">{totalIncomes} $</Descriptions.Item>
-    <Descriptions.Item label="Total Expenses">{totalExpenses} $</Descriptions.Item>
-    <Descriptions.Item label="Balance at"> Date </Descriptions.Item>
-    <Descriptions.Item label="Total"> {balance} $ </Descriptions.Item>
+    {/* <Descriptions.Item label="UserName" style= {{fontWeight:'bolder'}}> Marce Griet</Descriptions.Item> */}
+    <Descriptions.Item label="Total Incomes"> {totalIncomes}$</Descriptions.Item>
+    <Descriptions.Item label="Total Expenses"> {totalExpenses}$</Descriptions.Item>
+    {/* <Descriptions.Item label="Balance at"> Date </Descriptions.Item> */}
+    <Descriptions.Item label="Total Balance"> {balance} $ </Descriptions.Item>
   </Descriptions>
     )
 }
